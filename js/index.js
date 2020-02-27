d593b8b1555b5d254887ce57f2ccd83e1c6ef98b
AtakanFire <atakanates06@gmail.com>
2020-02-28 01:22:00 +0300

    Styles updated!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Index.js
+
+// Menu Open Close
+var burger = document.querySelector('.burger');
+var menu = document.querySelector('#'+ burger.dataset.target);
+burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+});
+
+
+// Sticky navbar
+var navbar = document.querySelector(".navbar");
+var sticky = navbar.offsetTop;
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("is-fixed-top")
+    } else {
+        navbar.classList.remove("is-fixed-top");
+    }
+});
