507f45abd083c5cea9375abbf2bb805eab173051
AtakanFire <atakanates06@gmail.com>
2020-04-15 22:48:14 +0300

    Assets added!

--- js/index.js
+++ js/index.js
@@ -34,3 +34,35 @@ function animateCSS(element, animationName, callback) {
 
     node.addEventListener('animationend', handleAnimationEnd)
 }
+
+// Carousel(SlideShow)
+function getCurrentCarouselSlide() {
+    var dots = document.getElementsByClassName("carousel-dot");
+    var finded = 0;
+    for (i = 0; i < dots.length; i++) {
+        const currentDot = dots[i];      
+        if (currentDot.classList.contains("carousel-active-dot")) {
+            finded = i;
+        }
+    }
+    return finded;
+}
+function showCarouselSlide(carouselSlide) {
+    var i;
+    var slides = document.getElementsByClassName("carousel-slide");
+    var dots = document.getElementsByClassName("carousel-dot");
+    var slideIndex = (carouselSlide !== '++' && carouselSlide !== '--' ? carouselSlide : (getCurrentCarouselSlide() + 1));
+    if (carouselSlide === '++') { slideIndex++ }
+    if (carouselSlide === '--') { slideIndex-- }
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" carousel-active-dot", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " carousel-active-dot";
+    console.log(slideIndex + " : " + slides.length)
+}
